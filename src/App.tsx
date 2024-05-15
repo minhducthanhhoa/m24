23e4651ba1f8c3da06b17ed8749a83a90b090eed
minhducthanhhoa <ngominhduc2705@gmail.com>
2024-05-15 10:06:42 +0700

    baitap

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Function from './components/functionComponents/Function'
+import Class from './components/classComponents/Class'
+export default function App() {
+ 
+  return (
+    <div>
+
+      <Function></Function>
+      <Class></Class>
+    </div>
+  )
+}
